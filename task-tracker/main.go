7391fa8dd68c6d2fefcf2cd09657a46c20b6b97d
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-11-17 13:23:25 +0000

    docs: Add documentation to the main function and a TODO for later

--- task-tracker/main.go
+++ task-tracker/main.go
@@ -7,18 +7,21 @@ import (
 	"github.com/nanafox/task-tracker/cmd"
 )
 
+// TODO: Include a good help system for task-cli so users know what to expect
+// when using it.
+
+// main provides the entry to the task-cli program. It accepts the CLI arguments
+// and hands them over to the Execute function which takes it from there.
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Fprintln(os.Stderr, "Usage: task-cli <cmd>")
 		os.Exit(1)
 	}
 
-	taskId, err := cmd.Execute(os.Args[1:])
+	_, err := cmd.Execute(os.Args[1:])
 
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-
-	fmt.Printf("Task created with ID: %d", taskId)
 }
