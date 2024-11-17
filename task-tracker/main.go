d9d477e899e3a003082cccb56b698928855f3a19
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-11-17 16:16:54 +0000

    chore: Remove TODO action

--- task-tracker/main.go
+++ task-tracker/main.go
@@ -7,9 +7,6 @@ import (
 	"github.com/nanafox/task-tracker/cmd"
 )
 
-// TODO: Include a good help system for task-cli so users know what to expect
-// when using it.
-
 // main provides the entry to the task-cli program. It accepts the CLI arguments
 // and hands them over to the Execute function which takes it from there.
 func main() {
