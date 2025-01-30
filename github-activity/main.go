229f2ac726ca15820f3677414a61b7e14e05ac53
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-01-30 15:16:13 +0000

    chore: Remove whitespace from usage info

--- github-activity/main.go
+++ github-activity/main.go
@@ -15,16 +15,16 @@ import (
 
 const usage = `Usage: github-activity <username> [number of events: Default is 10],
 
-  github-activity is a simple CLI tool to fetch the latest activities a user 
-  is performing with their GitHub account. It returns the results in a simple 
+  github-activity is a simple CLI tool to fetch the latest activities a user
+  is performing with their GitHub account. It returns the results in a simple
   listing format.
 
   For efficiency, responses are cached for later in the /tmp/github-activity-cache/
   directory. If you need to get fresh updates for the same user, feel free to
   delete the cached files for that user or all users if you prefer.
 
-  The cache files are saved in this format, <username>_<page_size>. So as an 
-  example running 'github-activity nanafox 10' will return the first 10 events 
+  The cache files are saved in this format, <username>_<page_size>. So as an
+  example running 'github-activity nanafox 10' will return the first 10 events
   of the 'nanafox' user. The resultant cache file will be 'nanafox_10.json'
 `
 
